6339b416429bd44173b740356f2e570a6d8dd1e9
Aivaras Jonikas <aivarasjonikas@gmail.com>
2020-06-01 21:52:57 +0100

    Initial network monitor client commit

---
+++ src/NetworkMonitor.Test.Unit/SpeedTesterTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetworkMonitor.Test.Unit
+{
+  public class SpeedTesterTests
+  {
+    private BandwidthTester _speedTester;
+    private const string _fileDownloadUrl = "http://localhost:4000/download";
+
+    public SpeedTesterTests()
+    {
+      _speedTester = new BandwidthTester(new HttpClient(), _fileDownloadUrl);
+    }
+
+
+    [Fact]
+    public async Task GetDownloadSpeed_ValidUrl()
+    {
+      var result = await _speedTester.GetDownloadSpeed();
+    }
+
+    
+  }
+}
